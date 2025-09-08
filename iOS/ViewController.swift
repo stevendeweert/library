6525b3dd435b695a90fde825725a4c6a3a870f92
Steven De Weert <git@stevendeweert.be>
2025-09-08 13:41:12 +0200

    Added iOS target

---
+++ iOS/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOS
+//
+//  Created by Steven De Weert on 08/09/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemBackground
+        
+    }
+
+
+}
+
